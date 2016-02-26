ced3855e43a643ac798cf8e84e23f456c8c14d5d
mheob <mail@alex-boehm.de>
2016-02-26 09:46:00 +0100

    move "Utils" to "SHD.KPSA.Utils"

--- SHD.KPSA.Utils/Converter/BaseConverter.cs
+++ SHD.KPSA.Utils/Converter/BaseConverter.cs
@@ -3,6 +3,10 @@
     using System;
     using System.Windows.Markup;
 
+    /// <summary>
+    /// A BaseConverter class to should easier implements converter in the XAML-code.
+    /// values.
+    /// </summary>
     public abstract class BaseConverter : MarkupExtension
     {
         public override object ProvideValue(IServiceProvider serviceProvider)
