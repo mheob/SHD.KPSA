cb868c61b80fed880b6156db14448c3cea7062e7
mheob <mail@alex-boehm.de>
2016-02-29 15:44:47 +0100

    added converter to the collection

---
+++ SHD.KPSA.Utils/Converter/InverseBoolConverter.cs
@@ -0,0 +1,24 @@
+﻿
+#pragma warning disable 1591
+
+namespace SHD.KPSA.Utils.Converter
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class InverseBoolConverter : IValueConverter
+    {
+        #region IValueConverter Members
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool) value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
