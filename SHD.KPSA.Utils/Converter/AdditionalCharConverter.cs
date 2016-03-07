66cd527d06072cd7e87fbdcaf8f9947c5fc5b565
mheob <mail@alex-boehm.de>
2016-03-07 20:13:38 +0100

    added a converter for replacing special characters

---
+++ SHD.KPSA.Utils/Converter/AdditionalCharConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace SHD.KPSA.Utils.Converter
+{
+    /// <summary>
+    /// Class for converting chars to special characters
+    /// </summary>
+    public static class AdditionalCharConverter
+    {
+        /// <summary>
+        /// Converts special characters in ASCII code.
+        /// </summary>
+        /// <param name="s">The string to be converted.</param>
+        /// <returns>The converted string.</returns>
+        public static string ConvertCharsToAscii(string s)
+        {
+            s = s.ToLower();
+
+            s = s.Replace("ä", "ae");
+            s = s.Replace("ö", "oe");
+            s = s.Replace("ü", "ue");
+            s = s.Replace("ß", "ss");
+            s = s.Replace("&", "_");
+            s = s.Replace("-", "_");
+            s = s.Replace(" ", "_");
+
+            return s;
+        }
+    }
+}
